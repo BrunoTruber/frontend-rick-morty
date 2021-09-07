b1036c439efc584e119aa40be78be693b5559e7e
BrunoTruber <brunotruber45@gmail.com>
2021-09-06 23:43:32 -0300

    ok

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 export const Api = {
-	baseUrl: "https://backend-rick-morty-bruno.herokuapp.com/personagens",
+	baseUrl: "https://backend-rick-morty-bruno.herokuapp.com",
 
 	createUrl: () => Api.baseUrl + "personagens/create",
 
